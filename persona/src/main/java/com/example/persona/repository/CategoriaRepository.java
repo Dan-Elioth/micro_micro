be359f9b3ca5150aa59a46b2aaf04bfd2c991ac9
Dan-Elioth <elitocp@gmail.com>
2023-05-14 13:41:10 -0500

    asdasd

--- persona/src/main/java/com/example/persona/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.persona.repository;
-
-import com.example.persona.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
--- persona/src/main/java/com/example/persona/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.persona.repository;
-
-import com.example.persona.entity.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto,Integer> {
-
-}
