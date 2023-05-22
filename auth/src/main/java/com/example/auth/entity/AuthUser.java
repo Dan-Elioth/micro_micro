d80f8d76c50cf6a2c6431c601420825d95ba713f
Dan-Elioth <elitocp@gmail.com>
2023-05-22 11:31:04 -0500

    Ahorasi

--- auth/src/main/java/com/example/auth/entity/AuthUser.java
+++ auth/src/main/java/com/example/auth/entity/AuthUser.java
@@ -1,18 +1,24 @@
 package com.example.auth.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
+@Entity
 public class AuthUser {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private int id;
     private String userName;
     private String password;
 }
--- auth/src/main/java/com/example/auth/repository/AuthRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.auth.repository;
-
-import com.example.cliente.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthRepository extends JpaRepository<Auth,Integer> {
-}
